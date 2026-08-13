6bd1fee6ae297279f684d74425e57b326d27a207
kkagado70-cmd <kkagado70@gmail.com>
2026-08-13 01:29:59 -0300

    Update and rename src/main/java/com/example/ExampleMod.java to src/MatrixMod.java

---
+++ src/MatrixMod.java
@@ -0,0 +1,10 @@
+package com;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MatrixMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Matrix Client (1.21.x) - Mod Carregado!");
+    }
+}
